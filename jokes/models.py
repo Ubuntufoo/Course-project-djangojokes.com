30b3907b3e9e825076de1aa8e415530eb8d460ff
Timothy Murphy <Timmurphy123@gmail.com>
2023-02-01 13:47:53 -0500

    Created first data model and jokes

--- jokes/models.py
+++ jokes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Joke(models.Model):
+    question = models.TextField(max_length=200)
+    answer = models.TextField(max_length=100, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.question

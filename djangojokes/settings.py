4d1099e6ded4ba4d92ae0428005e63a971c1fa90
Timothy Murphy <Timmurphy123@gmail.com>
2023-02-04 17:10:39 -0500

    created app: users; swapped databases

--- djangojokes/settings.py
+++ djangojokes/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'common.apps.CommonConfig',
     'jokes.apps.JokesConfig',
     'pages.apps.PagesConfig',
+    'users.apps.UsersConfig',
 
 ]
 
@@ -83,8 +84,12 @@ WSGI_APPLICATION = 'djangojokes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'jokes',
+        'USER': 'postgres',
+        'PASSWORD': '7777',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 
@@ -106,6 +111,9 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+# AUTHENTICATION SETTINGS
+AUTH_USER_MODEL = 'users.CustomUser'
 
 
 # Internationalization

94fd359332b0252226cbd22330707f288007730d
Timothy Murphy <Timmurphy123@gmail.com>
2023-02-05 15:00:18 -0500

    Added Joke model to /admin, added admin docs

--- djangojokes/urls.py
+++ djangojokes/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('admin/doc/', include('django.contrib.admindocs.urls')),
     path('admin/', admin.site.urls),
     path('jokes/', include('jokes.urls')),
     path('', include('pages.urls')),
--- jokes/admin.py
+++ jokes/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Joke
+
+
+@admin.register(Joke)
+class JokeAdmin(admin.ModelAdmin):
+    model = Joke
+    list_display = ['question', 'created', 'updated']
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:  # editing an existing object
+            return ('created', 'updated')
+
+        return ()

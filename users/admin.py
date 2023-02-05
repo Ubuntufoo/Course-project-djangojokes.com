880ee4cf8e06680fd6de3cb6a419bf790fdf148f
Timothy Murphy <Timmurphy123@gmail.com>
2023-02-05 14:42:47 -0500

    Registered first superuser for djangojokes.com

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+CustomUser = get_user_model()
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Optional Fields', {
+            'classes': ('wide',),
+            'fields': ('email', 'first_name', 'last_name'),
+        }),
+    )

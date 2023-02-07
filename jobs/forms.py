aaaa45f8324aa3e6adb1aa1c44b3a4f6c557fc20
Timothy Murphy <Timmurphy123@gmail.com>
2023-02-07 18:01:50 -0500

    Changed widgets in forms.py

--- jobs/forms.py
+++ jobs/forms.py
@@ -1,7 +1,63 @@
+from datetime import datetime
 from django import forms
 
 
 class JobApplicationForm(forms.Form):
-    first_name = forms.CharField()
+    EMPLOYMENT_TYPES = (
+        (None, '--Please choose--'),
+        ('ft', 'Full-time'),
+        ('pt', 'Part-time'),
+        ('contract', 'Contract work')
+    )
+
+    DAYS = (
+        (1, 'MON'),
+        (2, 'TUE'),
+        (3, 'WED'),
+        (4, 'THU'),
+        (5, 'FRI')
+    )
+
+    YEARS = range(datetime.now().year, datetime.now().year+2)
+
+    first_name = forms.CharField(
+        widget=forms.TextInput(attrs={'autofocus': True})
+    )
     last_name = forms.CharField()
     email = forms.EmailField()
+    website = forms.CharField(
+        required=False,
+        widget=forms.TextInput(
+            attrs={'placeholder': 'https://www.example.com', 'size': '50'}
+        ),
+        validators=[URLValidator(schemes=['http', 'https'])]
+    )
+    employment_type = forms.ChoiceField(choices=EMPLOYMENT_TYPES)
+    start_date = forms.DateField(
+        help_text='The earliest date you can start working.',
+        widget=forms.SelectDateWidget(
+            years=YEARS,
+            attrs={'style': 'width: 31%; display: inline-block; margin: 0 1%'}
+        ),
+        validators=[validate_future_date],
+        error_messages={'past_date': 'Please enter a future date.'}
+    )
+    available_days = forms.TypedMultipleChoiceField(
+        choices=DAYS,
+        coerce=int,
+        help_text='Check all days that you can work.',
+        widget=forms.CheckboxSelectMultiple(
+            attrs={'checked': True}
+        )
+    )
+    desired_hourly_wage = forms.DecimalField(
+        widget=forms.NumberInput(
+            attrs={'min': '10.00', 'max': '100.00', 'step': '.25'}
+        )
+    )
+    cover_letter = forms.CharField(
+        widget=forms.Textarea(attrs={'cols': '75', 'rows': '5'})
+    )
+    confirmation = forms.BooleanField(
+        label='I certify that the information I have provided is true.'
+    )

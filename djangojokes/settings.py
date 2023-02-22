cb4f839ad4436894a360ed823265673328be2110
Timothy Murphy <Timmurphy123@gmail.com>
2023-02-22 16:58:26 -0500

    Added email templates; custom user regi form

--- djangojokes/settings.py
+++ djangojokes/settings.py
@@ -157,6 +157,8 @@ ACCOUNT_LOGIN_ATTEMPTS_TIMEOUT = 300 # Default 300
 ACCOUNT_LOGOUT_REDIRECT_URL = 'account_login' # Default: '/'
 ACCOUNT_USERNAME_REQUIRED = False # Default: True
 
+ACCOUNT_SIGNUP_FORM_CLASS = 'users.forms.SignupForm'
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/

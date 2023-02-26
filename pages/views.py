55f94ac8c4df76a11d95e33cde59a0b4630b7eb2
Timothy Murphy <Timmurphy123@gmail.com>
2023-02-26 13:16:57 -0500

    messages added to views

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import TemplateView
+from django.contrib import messages
 
 
 class HomePageView(TemplateView):
@@ -7,3 +8,11 @@ class HomePageView(TemplateView):
 
 class AboutUsView(TemplateView):
     template_name = 'pages/about_us.html'
+
+    def get(self, request, *args, **kwargs):
+        messages.debug(request, 'Debug message.')
+        messages.info(request, 'Info message.')
+        messages.success(request, 'Success message.')
+        messages.warning(request, 'Warning message.')
+        messages.error(request, 'Error message.')
+        return super().get(request, args, kwargs)
--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 from django.views.generic import UpdateView
 
@@ -10,9 +11,10 @@ from .forms import CustomUserChangeForm
 class CustomPasswordChangeView(LoginRequiredMixin, PasswordChangeView):
     success_url = reverse_lazy('my-account')
 
-class MyAccountPageView(LoginRequiredMixin, UpdateView):
+class MyAccountPageView(SuccessMessageMixin, LoginRequiredMixin, UpdateView):
     model = get_user_model()
     form_class = CustomUserChangeForm
+    success_message = 'Update Successful'
     template_name = 'account/my_account.html'
 
     def get_object(self):

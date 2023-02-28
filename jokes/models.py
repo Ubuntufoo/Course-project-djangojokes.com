0cf3cac63d5c7676808c1344c0be7e68e6fe5e57
Timothy Murphy <Timmurphy123@gmail.com>
2023-02-28 17:44:57 -0500

    fixed attributes location in joke model

--- jokes/models.py
+++ jokes/models.py
@@ -20,6 +20,18 @@ class Joke(models.Model):
     )
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    @property
+    def num_votes(self):
+        return self.jokevotes.count()
+
+    @property
+    def num_likes(self):
+        return self.jokevotes.filter(vote=1).count()
+
+    @property
+    def num_dislikes(self):
+        return self.jokevotes.filter(vote=-1).count()
     
 
     def get_absolute_url(self):
@@ -96,18 +108,6 @@ class JokeVote(models.Model):
     vote = models.SmallIntegerField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-
-    @property
-    def num_votes(self):
-        return self.jokevotes.count()
-
-    @property
-    def num_likes(self):
-        return self.jokevotes.filter(vote=1).count()
-
-    @property
-    def num_dislikes(self):
-        return self.jokevotes.filter(vote=-1).count()
 
     class Meta:
         constraints = [

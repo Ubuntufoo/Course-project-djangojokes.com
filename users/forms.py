51b6d7011b8650e800bec301aadea7c203a43df7
Timothy Murphy <Timmurphy123@gmail.com>
2023-02-28 14:52:24 -0500

    models.ImageField for user avatars

--- users/forms.py
+++ users/forms.py
@@ -21,7 +21,7 @@ class CustomUserChangeForm(UserChangeForm):
     
     class Meta:
         model = get_user_model()
-        fields = ('email', 'username', 'first_name', 'last_name', 'dob')
+        fields = ('email', 'username', 'first_name', 'last_name', 'dob', 'avatar')
         widgets = {
             'dob': forms.SelectDateWidget(
                 attrs={
